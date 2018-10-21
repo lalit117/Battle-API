0b54d3e4847b0ed1b5f8436cee5ada7b0f6cec11
lm117 <lalitmunot117@gmail.com>
2018-10-21 23:47:02 +0530

    Modified package.json

--- config/config.js
+++ config/config.js
@@ -5,10 +5,6 @@ console.log('Current Environment : ' +  env);
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/GOT';
-} 
-// else if (env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/todos-test';
-// }
+}
 
 console.log('MongoDb Path : ' +  process.env.MONGODB_URI);
